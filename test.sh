46f76069d9dae842ce343a16863494dcf15851bd
bwebb3234 <bwebb3239@gmail.com>
2025-03-12 08:13:29 -0800

    Update

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 :'
 create a script that does a back up of a specific directory passed through the scripts through arguments
 this script will function based off parameters passed to the script through the command line.
+
+one job could be thejiggle set to run every 25 minutes, it runs a script that performs different tasks based on the arguments
+passed to the script through the command line.
 '
 
 
