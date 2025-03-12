c8189101c8d14f558b7fc1c5cb7103165ea24c96
bwebb3234 <bwebb3239@gmail.com>
2025-03-12 08:07:46 -0800

    Update

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+:'
+create a script that does a back up of a specific directory passed through the scripts through arguments
+this script will function based off parameters passed to the script through the command line.
+'
+
 
 for arg in "$@"
 do
